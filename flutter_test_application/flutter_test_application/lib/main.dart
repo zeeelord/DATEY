4a9a73b06d245d589f65a5dd8dafd40d04e5c6a5
Oladayo <oladayoafolabi@outlook.com>
2026-01-03 11:50:14 +0000

    refractoring code base

--- flutter_test_application/flutter_test_application/lib/main.dart
+++ flutter_test_application/flutter_test_application/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test_application/login_page.dart';
+// import 'package:flutter_test_application/login_page.dart';
 
 void main() {
   runApp(MyApp());
