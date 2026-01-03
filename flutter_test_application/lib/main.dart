44dcd5cf05e77253e3b0bd0516da351758f7803f
Oladayo <oladayoafolabi@outlook.com>
2026-01-03 12:46:19 +0000

    refactoring dirty code base

--- flutter_test_application/lib/main.dart
+++ flutter_test_application/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_application/login_page.dart';
 // import 'package:flutter_test_application/login_page.dart';
 
 void main() {
@@ -10,45 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        // appBar: AppBar(),
-        // floatingActionButton: FloatingActionButton(onPressed: null),
-        backgroundColor: Colors.blue.shade800,
-        // drawer: Container(height: 500, width: 400, color: Colors.white),
-        body: Column(
-          children: [
-            Text("Hello welcome back!"),
-            Text('Login to continue'),
-            TextField(decoration: InputDecoration(hintText: 'Username')),
-            TextField(decoration: InputDecoration(hintText: 'Password')),
-            TextButton(
-              onPressed: () {
-                print("Clicked");
-              },
-              child: Text('Forgot password?'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print("login is clicked");
-              },
-              child: Text('Log in '),
-            ),
-            Text('Or sign in with'),
-            ElevatedButton(
-              onPressed: () {
-                print(" google  is clicked");
-              },
-              child: Image.asset(
-                'assets/images/facebook_icon.jpg',
-                width: 22,
-                height: 22,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: loginPage());
   }
 }
