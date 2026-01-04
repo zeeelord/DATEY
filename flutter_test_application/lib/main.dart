b7e9b0648dd1be1dc6664dd9733b84caa461ddff
Oladayo <oladayoafolabi@outlook.com>
2026-01-04 18:13:28 +0000

    Add AppColors class for centralized color management in the application

--- flutter_test_application/lib/main.dart
+++ flutter_test_application/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test_application/login_page.dart';
+import 'package:flutter_test_application/styles/app_colors.dart';
 // import 'package:flutter_test_application/login_page.dart';
 
 void main() {
@@ -10,6 +11,13 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: loginPage());
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Roboto',
+        scaffoldBackgroundColor: AppColors.background,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: loginPage(),
+    );
   }
 }
---
+++ flutter_test_application/lib/styles/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xffFBD512);
+  static const font = Color(0xffd8d8d8);
+  static const font2 = Color(0xff373737);
+  static const disableFont = Color(0xffa7a7a7);
+  static const disableButton = Color(0xffFBD512);
+  static const background = Color(0xff1A2947);
+  static const black = Color(0xff000000);
+  static const white = Color(0xffffffff);
+}
