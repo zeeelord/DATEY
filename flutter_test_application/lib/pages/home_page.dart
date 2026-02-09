6650899d8ff361a5895830f75e507cd747261d10
Oladayo <zeelord82@gmail.com>
2026-02-09 01:36:12 +0000

    Add homepage and login page with UI components
    - Created a new Homepage widget with a simple text display.
    - Implemented a login page with text fields for username and password.
    - Added buttons for login and social media sign-in options.
    - Included navigation to the homepage upon successful login.

---
+++ flutter_test_application/lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Center(child: Text('Home page')) 
+    );
+  }
+}
